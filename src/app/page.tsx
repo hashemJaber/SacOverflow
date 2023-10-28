c40d4683b8061086fc3cff4ccf2056cacbb14f08
Junior <49306278+jvniorrr@users.noreply.github.com>
2023-10-27 17:33:44 -0700

    SO-2 homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,118 +1,81 @@
+import Card from '@/components/HomePage/Card';
 import Image from 'next/image';
+import Buttons from '@/components/SharedComponents/Buttons';
+
+import { Lexend_Giga } from 'next/font/google';
+
+const inter = Lexend_Giga({ subsets: ['latin'] });
+
+const cardOneContent = (
+    <p className="leading-6">
+        <b>Cloud Asset Management Enhanced Launcher (CAMEL)</b>
+        is aimed towards assisting Project Managers in running and managing
+        their businesses efficiently. Through the use of this application,
+        Business Owners will be able to make wiser and informed decisions
+    </p>
+);
+
+const cardTwoContent = (
+    // <div>
+    <ul className="list-disc leading-6">
+        <li>All-In-One Dashboard</li>
+        <li>Document Management</li>
+        <li>Time Tracking and Management</li>
+        <li>Reporting and Visualization Tools</li>
+        <li>Task Prioritization and Scheduling</li>
+        <li>Business Overview Analytics</li>
+        <li>Project’s Overviews</li>
+    </ul>
+    // {/* </div> */}
+);
+
+const cardThreeContent = (
+    <div className="flex flex-col gap-y-2 justify-center items-center">
+        <Image
+            src={'/images/hashemtmp.jpeg'}
+            width={100}
+            height={100}
+            alt="testimonial-image"
+            className="rounded-full w-20 h-20"
+        />
+        {/* <div className="testimonial-name"> */}
+        <h5 className="testiominal-name font-bold text-primary-green-700">
+            Hashem Jaber
+        </h5>
+
+        <div className="testimonial-text">
+            <p>
+                CAMEL has been instrumental in enhancing our product management
+                workflow. The intuitive dashboards and analytical tools have
+                enabled me to make data-driven decisions that have significantly
+                enhanced my projects success rates
+            </p>
+        </div>
+        {/* </div> */}
+    </div>
+);
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    Get started by editing&nbsp;
-                    <code className="font-mono font-bold">
-                        src/app/page.tsx
-                    </code>
-                </p>
-                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-                    <a
-                        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        By{' '}
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className="dark:invert"
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
+        <main className="flex min-h-screen flex-col items-center justify-between">
+            <section id="home-section">
+                <div id="image-content">
+                    <div className={inter.className}>
+                        <h3 className="page-title">
+                            Cloud Asset Management Enhanced Launcher
+                        </h3>
+                    </div>
+                    <div className="flex flex-col md:flex-row gap-2 p-24 items-center md:items-stretch">
+                        <Card title="Our Product" content={cardOneContent} />
+                        <Card title="Features" content={cardTwoContent} />
+                        <Card title="Testimonials" content={cardThreeContent} />
+                    </div>
                 </div>
-            </div>
-
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-                <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
-            <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Docs{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Find in-depth information about Next.js features and
-                        API.
-                    </p>
-                </a>
-
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Learn{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Learn about Next.js in an interactive course
-                        with&nbsp;quizzes!
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Templates{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Explore the Next.js 13 playground.
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Deploy{' '}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Instantly deploy your Next.js site to a shareable URL
-                        with Vercel.
-                    </p>
-                </a>
-            </div>
+                {/* join us btn */}
+                <div className="join-us flex justify-center mt-6">
+                    <Buttons variant="primary" size="large" content="Join Us" />
+                </div>
+            </section>
         </main>
     );
 }

1e82cb3fdfc800d0caa59496d9593cc6ceaa5601
Junior <49306278+jvniorrr@users.noreply.github.com>
2023-11-07 02:45:26 -0800

    SO-20 Layout Format, Component Tweaks & Dashboard Page
    SO-20 Layout Format, Component Tweaks & Dashboard Page
    
    Fixed Web App Layout
    Fixed minor tweaks to Components for responsiveness

--- src/components/Dashboard/MonthlySpend.tsx
+++ src/components/Dashboard/MonthlySpend.tsx
@@ -1,51 +1,61 @@
 //Import CSS file for Monthly Spending styles
-import "./MonthlySpending.css";
+import './MonthlySpending.css';
 //Interface categorizing data by its type
 interface SpendingData {
-  category: string;
-  amount: number;
-  total: number;
-  trend: "up" | "down";
+    category: string;
+    amount: number;
+    total: number;
+    trend: 'up' | 'down';
 }
 //Function to format numbers with commas for thousands
 function formatNumber(num: number): string {
-  return num.toLocaleString();
+    return num.toLocaleString();
 }
 //Functional Component to display monthly spending data
 const MonthlySpending = () => {
-  //Example data of spending data array
-  const data: SpendingData[] = [
-    { category: "Electrical", amount: 760, total: 2000, trend: "up" },
-    { category: "Rental", amount: 760, total: 2000, trend: "up" },
-    { category: "Plumbing", amount: 760, total: 2000, trend: "down" },
-    { category: "Construction", amount: 760, total: 2000, trend: "up" },
-    { category: "HVAC", amount: 760, total: 2000, trend: "down" },
-    { category: "Materials", amount: 760, total: 2000, trend: "up" },
-  ];
+    //Example data of spending data array
+    const data: SpendingData[] = [
+        { category: 'Electrical', amount: 760, total: 2000, trend: 'up' },
+        { category: 'Rental', amount: 760, total: 2000, trend: 'up' },
+        { category: 'Plumbing', amount: 760, total: 2000, trend: 'down' },
+        { category: 'Construction', amount: 760, total: 2000, trend: 'up' },
+        { category: 'HVAC', amount: 760, total: 2000, trend: 'down' },
+        { category: 'Materials', amount: 760, total: 2000, trend: 'up' },
+    ];
 
-  return (
-    <div className="monthly-spending-container">
-      {/* Title for Monthly Spending */}
-      <div className="spending-title"> Monthly Spending </div>
-      {/* Container for the list of data */}
-      <div className="spending-container">
-        {/* Unordered list to map out data, displays each spending category and amount, formatted into grid with commas */}
-        <ul>
-          {data.map((item, index) => (
-            <li key={index} className="spending-list-item">
-              <span className="category">{item.category}</span>
-              <span className="amount">{formatNumber(item.amount)}</span>
-              <span className="total">{formatNumber(item.total)}</span>
-              {/* Displays the trend, whether the icon is up or down */}
-              <span className={item.trend === "up" ? "trend-up" : "trend-down"}>
-                {item.trend === "up" ? "▲" : "▼"}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <div className="monthly-spending-container">
+            {/* Title for Monthly Spending */}
+            {/* <div className="spending-title"> Monthly Spending </div> */}
+            {/* Container for the list of data */}
+            <div className="spending-container">
+                {/* Unordered list to map out data, displays each spending category and amount, formatted into grid with commas */}
+                <ul>
+                    {data.map((item, index) => (
+                        <li key={index} className="spending-list-item">
+                            <span className="category">{item.category}</span>
+                            <span className="amount">
+                                {formatNumber(item.amount)}
+                            </span>
+                            <span className="total">
+                                {formatNumber(item.total)}
+                            </span>
+                            {/* Displays the trend, whether the icon is up or down */}
+                            <span
+                                className={
+                                    item.trend === 'up'
+                                        ? 'trend-up'
+                                        : 'trend-down'
+                                }
+                            >
+                                {item.trend === 'up' ? '▲' : '▼'}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default MonthlySpending;
--- src/components/Dashboard/revenue_linechart/linechart.tsx
+++ src/components/Dashboard/revenue_linechart/linechart.tsx
@@ -1,175 +1,194 @@
 'use client';
 
 import React, { useRef, useLayoutEffect } from 'react';
-import * as am5 from "@amcharts/amcharts5";
-import * as am5xy from "@amcharts/amcharts5/xy";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
-import am5themes_Responsive from "@amcharts/amcharts5/themes/Responsive"; 
+import * as am5 from '@amcharts/amcharts5';
+import * as am5xy from '@amcharts/amcharts5/xy';
+import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
 
-function LineChart() {
-  const chartRef = useRef(null);
+function LineChart(props) {
+    let { id, className } = props;
+    const chartRef = useRef(null);
 
-  useLayoutEffect(() => {
-    let root = am5.Root.new("chartdiv");
+    useLayoutEffect(() => {
+        let root = am5.Root.new(`${id}`);
 
-    root.setThemes([
-        am5themes_Animated.new(root),
-        // am5themes_Responsive.new(root, {
-        //   rules: [
-        //     {
-        //       // When on a small width device like mobile
-        //       relevant: (target) => target.pixelWidth <= 600,
-        //       state: (target, stateId) => {
-        //         if (target instanceof am5xy.CategoryAxis) {
-        //           target.get("renderer").labels.template.setAll({
-        //             fontSize: 10, // Reduce the font size
-        //             rotation: -45, // Rotate labels to prevent overlap
-        //             centerY: am5.p50,
-        //             centerX: am5.p100
-        //           });
-        //           target.get("renderer").minGridDistance = 50; // Increase the grid distance
-        //         }
-        //         if (target instanceof am5xy.ValueAxis) {
-        //           target.get("renderer").labels.template.setAll({
-        //             fontSize: 10 // Reduce the font size
-        //           });
-        //         }
-        //         if (target instanceof am5.Circle) {
-        //           // If this is a bullet, reduce its size
-        //           target.setAll({ radius: 2 });
-        //         }
-        //         return stateId;
-        //       }
-        //     },
-        //     {
-        //       // When on a medium width device
-        //       relevant: (target) => target.pixelWidth > 600 && target.pixelWidth <= 800,
-        //       state: (target, stateId) => {
-        //         if (target instanceof am5xy.CategoryAxis) {
-        //           target.get("renderer").labels.template.setAll({
-        //             fontSize: 12, // Slightly larger font size
-        //             rotation: -45, // Rotate labels to prevent overlap
-        //             centerY: am5.p50,
-        //             centerX: am5.p100
-        //           });
-        //           target.get("renderer").minGridDistance = 40; // Slightly reduce the grid distance
-        //         }
-        //         if (target instanceof am5xy.ValueAxis) {
-        //           target.get("renderer").labels.template.setAll({
-        //             fontSize: 12 // Slightly larger font size
-        //           });
-        //         }
-        //         return stateId;
-        //       }
-        //     }
-        //     // Add more rules as needed
-        //   ]
-        // })
-      ]);
+        root.setThemes([
+            am5themes_Animated.new(root),
+            // am5themes_Responsive.new(root, {
+            //   rules: [
+            //     {
+            //       // When on a small width device like mobile
+            //       relevant: (target) => target.pixelWidth <= 600,
+            //       state: (target, stateId) => {
+            //         if (target instanceof am5xy.CategoryAxis) {
+            //           target.get("renderer").labels.template.setAll({
+            //             fontSize: 10, // Reduce the font size
+            //             rotation: -45, // Rotate labels to prevent overlap
+            //             centerY: am5.p50,
+            //             centerX: am5.p100
+            //           });
+            //           target.get("renderer").minGridDistance = 50; // Increase the grid distance
+            //         }
+            //         if (target instanceof am5xy.ValueAxis) {
+            //           target.get("renderer").labels.template.setAll({
+            //             fontSize: 10 // Reduce the font size
+            //           });
+            //         }
+            //         if (target instanceof am5.Circle) {
+            //           // If this is a bullet, reduce its size
+            //           target.setAll({ radius: 2 });
+            //         }
+            //         return stateId;
+            //       }
+            //     },
+            //     {
+            //       // When on a medium width device
+            //       relevant: (target) => target.pixelWidth > 600 && target.pixelWidth <= 800,
+            //       state: (target, stateId) => {
+            //         if (target instanceof am5xy.CategoryAxis) {
+            //           target.get("renderer").labels.template.setAll({
+            //             fontSize: 12, // Slightly larger font size
+            //             rotation: -45, // Rotate labels to prevent overlap
+            //             centerY: am5.p50,
+            //             centerX: am5.p100
+            //           });
+            //           target.get("renderer").minGridDistance = 40; // Slightly reduce the grid distance
+            //         }
+            //         if (target instanceof am5xy.ValueAxis) {
+            //           target.get("renderer").labels.template.setAll({
+            //             fontSize: 12 // Slightly larger font size
+            //           });
+            //         }
+            //         return stateId;
+            //       }
+            //     }
+            //     // Add more rules as needed
+            //   ]
+            // })
+        ]);
 
-    // Create chart
-    let chart = root.container.children.push(am5xy.XYChart.new(root, {
-      panX: false,
-      panY: false,
-      wheelX: "panX",
-      wheelY: "zoomX",
-      pinchZoomX: true
-    }));
+        // Create chart
+        let chart = root.container.children.push(
+            am5xy.XYChart.new(root, {
+                panX: false,
+                panY: false,
+                wheelX: 'panX',
+                wheelY: 'zoomX',
+                pinchZoomX: true,
+            })
+        );
 
-    // Add cursor
-    let cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
-    cursor.lineY.set("visible", false);
+        // Add cursor
+        let cursor = chart.set('cursor', am5xy.XYCursor.new(root, {}));
+        cursor.lineY.set('visible', false);
 
-    // Create axes
-    let xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30 });
-    let xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
-      maxDeviation: 0.3,
-      categoryField: "date", 
-      renderer: xRenderer,
-      tooltip: am5.Tooltip.new(root, {})
-    }));
-
-    let yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-      renderer: am5xy.AxisRendererY.new(root, {})
-    }));
-
-    // Add series
-    function makeSeries(name, fieldName, color) {
-      let series = chart.series.push(am5xy.LineSeries.new(root, {
-        name: name,
-        xAxis: xAxis,
-        yAxis: yAxis,
-        valueYField: fieldName,
-        categoryXField: "date", 
-        stroke: am5.color(color),
-        tooltip: am5.Tooltip.new(root, {
-          labelText: "{valueY}"
-        })
-      }));
-
-      series.strokes.template.set("strokeWidth", 2);
-      series.data.setAll(chartData);
-
-      let bullet = series.bullets.push(function() {
-        return am5.Bullet.new(root, {
-          sprite: am5.Circle.new(root, {
-            radius: 4,
-            fill: am5.color(color),
-          })
+        // Create axes
+        let xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30 });
+        let xAxis = chart.xAxes.push(
+            am5xy.CategoryAxis.new(root, {
+                maxDeviation: 0.3,
+                categoryField: 'date',
+                renderer: xRenderer,
+                tooltip: am5.Tooltip.new(root, {}),
+                // font size
+            })
+        );
+        xAxis.get('renderer').labels.template.setAll({
+            fontSize: `.5rem`,
         });
-      });
 
-      chartRef.current = chart;
-    }
-    
+        let yAxis = chart.yAxes.push(
+            am5xy.ValueAxis.new(root, {
+                renderer: am5xy.AxisRendererY.new(root, {}),
+            })
+        );
+        yAxis.get('renderer').labels.template.setAll({
+            fontSize: `.6rem`,
+        });
 
-    // Set data
-    let chartData = [
-      {
-        date: new Date("2023-01-01"),
-        income: 6500,
-        expenses: 4800
-      },
-      {
-        date: new Date("2023-02-15"),
-        income: 5400,
-        expenses: 4100
-      },
-      {
-        date: new Date("2023-02-20"),
-        income: 6700,
-        expenses: 4300
-      },
-      {
-        date: new Date("2023-03-10"),
-        income: 7100,
-        expenses: 3900
-      },
-    ];
-    
+        // Add series
+        function makeSeries(name, fieldName, color) {
+            let series = chart.series.push(
+                am5xy.LineSeries.new(root, {
+                    name: name,
+                    xAxis: xAxis,
+                    yAxis: yAxis,
+                    valueYField: fieldName,
+                    categoryXField: 'date',
+                    stroke: am5.color(color),
+                    tooltip: am5.Tooltip.new(root, {
+                        labelText: '{valueY}',
+                    }),
+                })
+            );
 
-    // Create series for each category
-    makeSeries("Income", "income", "#34568B");
-    makeSeries("Expenses", "expenses", "#FF6F61");
+            series.strokes.template.set('strokeWidth', 2);
+            series.data.setAll(chartData);
 
-    // Add legend
-    let legend = chart.children.push(am5.Legend.new(root, {
-      centerX: am5.p50,
-      x: am5.p50
-    }));
-    legend.data.setAll(chart.series.values);
+            let bullet = series.bullets.push(function () {
+                return am5.Bullet.new(root, {
+                    sprite: am5.Circle.new(root, {
+                        radius: 4,
+                        fill: am5.color(color),
+                    }),
+                });
+            });
 
-    xAxis.data.setAll(chartData);
+            chartRef.current = chart;
+        }
 
-    return () => {
-      root.dispose();
-    };
-  }, []);
+        // Set data
+        let chartData = [
+            {
+                date: new Date('2023-01-01'),
+                income: 6500,
+                expenses: 4800,
+            },
+            {
+                date: new Date('2023-02-15'),
+                income: 5400,
+                expenses: 4100,
+            },
+            {
+                date: new Date('2023-02-20'),
+                income: 6700,
+                expenses: 4300,
+            },
+            {
+                date: new Date('2023-03-10'),
+                income: 7100,
+                expenses: 3900,
+            },
+        ];
 
-  return (
-    <div id="chartdiv" style={{ width: "100%", height: "300px" }}></div>
-  );
+        // Create series for each category
+        makeSeries('Income', 'income', '#34568B');
+        makeSeries('Expenses', 'expenses', '#FF6F61');
+
+        // Add legend
+        let legend = chart.children.push(
+            am5.Legend.new(root, {
+                centerX: am5.p50,
+                x: am5.p50,
+            })
+        );
+        legend.data.setAll(chart.series.values);
+
+        xAxis.data.setAll(chartData);
+
+        return () => {
+            root.dispose();
+        };
+    }, []);
+
+    // return <div id={`${id}`} style={{ width: '100%', height: '300px' }}></div>;
+    return (
+        <div
+            id={`${id}`}
+            className={`${className}`}
+            style={{ width: '100%', height: '20rem' }}
+        ></div>
+    );
 }
 
 export default LineChart;
-

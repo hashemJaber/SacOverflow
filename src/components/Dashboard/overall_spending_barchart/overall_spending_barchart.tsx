11c1e7ce47eb800aa08d0c4a2d3072b3e0b91e22
Junior <49306278+jvniorrr@users.noreply.github.com>
2023-11-07 02:45:26 -0800

    SO-20 Layout Format, Component Tweaks & Dashboard Page
    SO-20 Layout Format, Component Tweaks & Dashboard Page
    
    Fixed Web App Layout
    Fixed minor tweaks to Components for responsiveness

--- src/components/Dashboard/overall_spending_barchart/overall_spending_barchart.tsx
+++ src/components/Dashboard/overall_spending_barchart/overall_spending_barchart.tsx
@@ -1,93 +1,120 @@
 'use client';
 
 import React, { useEffect } from 'react';
-import * as am5 from "@amcharts/amcharts5";
-import * as am5percent from "@amcharts/amcharts5/percent";
-import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
+import * as am5 from '@amcharts/amcharts5';
+import * as am5percent from '@amcharts/amcharts5/percent';
+import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
 
-function PieChart() {
-  useEffect(() => {
-    // Create root element
-    let root = am5.Root.new("chartdiv1");
+interface PieChartDataElement {
+    value: number;
+    cataegory: string;
+}
 
-    
+function PieChart(props) {
+    const { className } = props;
+    useEffect(() => {
+        // Create root element
+        let root = am5.Root.new('chartdiv1');
 
-    // Set themes
-    root.setThemes([
-      am5themes_Animated.new(root)
-    ]);
+        // Set themes
+        root.setThemes([am5themes_Animated.new(root)]);
 
-    // Create chart
-    let chart = root.container.children.push(am5percent.PieChart.new(root, {
-      layout: root.verticalLayout
-    }));
+        // Create chart
+        let chart = root.container.children.push(
+            am5percent.PieChart.new(root, {
+                layout: root.verticalLayout,
+            })
+        );
 
-    // Create series
-    let series = chart.series.push(am5percent.PieSeries.new(root, {
-      alignLabels: true,
-      calculateAggregates: true,
-      valueField: "value",
-      categoryField: "category",
-      innerRadius: am5.percent(50) // This makes the chart a donut chart
-    }));
+        // Create series
+        let series = chart.series.push(
+            am5percent.PieSeries.new(root, {
+                alignLabels: true,
+                calculateAggregates: true,
+                valueField: 'value',
+                categoryField: 'category',
+                innerRadius: am5.percent(50), // This makes the chart a donut chart
+            })
+        );
 
-    series.slices.template.setAll({
-      strokeWidth: 3,
-      stroke: am5.color(0xffffff)
-    });
+        // remove text for labels
+        series.labels.template.setAll({
+            // remove labels
+            visible: false,
+        });
 
-    series.labelsContainer.set("paddingTop", 30);
+        series.slices.template.setAll({
+            strokeWidth: 3,
+            stroke: am5.color(0xffffff),
+        });
 
-    // Set data
-    series.data.setAll([{
-        value: 10,
-        category: "One"
-      }, {
-        value: 9,
-        category: "Two"
-      }, {
-        value: 6,
-        category: "Three"
-      }, {
-        value: 5,
-        category: "Four"
-      }, {
-        value: 4,
-        category: "Five"
-      }, {
-        value: 3,
-        category: "Six"
-      }]); // Add your data here
+        series.labelsContainer.set('paddingTop', 30);
 
-    // Create legend
-    let legend = chart.children.push(am5.Legend.new(root, {
-      centerX: am5.p50,
-      x: am5.p50,
-      marginTop: 15,
-      marginBottom: 15
-    }));
+        // Set data
+        series.data.setAll([
+            {
+                value: 10,
+                category: 'One',
+            },
+            {
+                value: 9,
+                category: 'Two',
+            },
+            {
+                value: 6,
+                category: 'Three',
+            },
+            {
+                value: 5,
+                category: 'Four',
+            },
+            {
+                value: 4,
+                category: 'Five',
+            },
+            {
+                value: 3,
+                category: 'Six',
+            },
+        ]); // Add your data here
 
-    // legend.data.setAll(series.dataItems);
+        // Create legend
+        let legend = chart.children.push(
+            am5.Legend.new(root, {
+                centerX: am5.p50,
+                x: am5.p50,
+                marginTop: 15,
+                marginBottom: 15,
+                visible: false,
+            })
+        );
 
-    // Add label for center text
-    let centerLabel = chart.seriesContainer.children.push(am5.Label.new(root, {
-      text: "Enter Text",
-      centerX: am5.percent(50),
-      centerY: am5.percent(50),
-      fontSize: 20
-    }));
+        // legend.data.setAll(series.dataItems);
 
-    // Play initial series animation
-    series.appear(1000, 100);
+        // Add label for center text
+        let centerLabel = chart.seriesContainer.children.push(
+            am5.Label.new(root, {
+                text: 'Enter Text',
+                centerX: am5.percent(50),
+                centerY: am5.percent(50),
+                fontSize: `.45rem`,
+            })
+        );
 
-    // Cleanup
-    return () => root.dispose();
-  }, []);
+        // Play initial series animation
+        series.appear(1000, 100);
 
-  return (
-    <div id="chartdiv1" style={{ width: "100%", height: "300px" }}></div>
-  );
+        // Cleanup
+        return () => root.dispose();
+    }, []);
+
+    return (
+        <div
+            id="chartdiv1"
+            className={`${className}`}
+            style={{ width: '100%', height: '100%' }}
+        ></div>
+    );
 }
 
 export default PieChart;
-

33c28140aeb4c854a016db7a8360c9a499ac50ce
Junior <49306278+jvniorrr@users.noreply.github.com>
2023-11-08 12:45:34 -0800

    SO-4 Added popup error for sign up and fixed bugs

---
+++ src/components/SignUp/PopUp.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./PopUp.css";
+
+const PopUp = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="popup-overlay">
+      <div className="popup-content">
+        <button className="popup-close" onClick={onClose}>
+          X
+        </button>
+        <p>
+          <h1>
+            <strong>Something went wrong</strong>
+          </h1>
+          Please ensure that all inputs have been typed in correctly.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PopUp;
